b417d10711f890547f7189186c9dcf7683dbf0ae
sikB <sikhovski@icloud.com>
2016-04-25 10:15:59 -0400

    Electric or Not

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+var mongoClient = mongo.MongoClient;
+var mongoUrl = 'mongodb://localhost:27017/electricOrNot';
+var db;
+
+mongoClient.connect(mongoUrl, function(error, database){
+	db = database;
+
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // var cars = db.collection('cars').insert({name: 'Buick'});
+  db.collection('cars').find({}).toArray(function(error,carResult){
+  	// console.log(carResult);
+  	// for(i=0; i<carResult.length; i++){
+  	// 	console.log(carResult[i].imageSrc);
+  	var getRandomImage = Math.floor(Math.random() * carResult.length);
+  	 res.render('index', { carImage: carResult[getRandomImage].imageSrc });
+  	// }
+  })
+  // console.log(cars);
 });
 
 module.exports = router;
